a1a40dcdf25bbd401a281aae3aa3e178764d1559
adtpdn <adtpdn@gmail.com>
2024-11-15 12:45:33 +0800

    add secrets

--- pixeleye.config.ts
+++ pixeleye.config.ts
@@ -1,4 +1,4 @@
 export default {
-  token: process.env.PIXELEYE_TOKEN || "your-token",
+  token: process.env.PIXELEYE_TOKEN || "pxi__S4IPzVcVqprGuj8qbyHdY33izUnFFVDG:ZIUrOA1rPL3Q79fDAWYI3",
   // Remove the endpoint configuration since we'll use Pixeleye's cloud service
 };
